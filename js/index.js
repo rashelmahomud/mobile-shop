c0af93d2c24bbd50286454f2e61a7cc8aa64c51a
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-03-01 02:04:36 +0600

    add js working

--- js/index.js
+++ js/index.js
@@ -26,9 +26,10 @@ const displayPhoneResult = phones => {
         <div class="card">
         <img src="${phone.image}" class="card-img-top w-25 mx-auto my-3" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${phone.brand}</h5>
-          <h3>${phone.phone_name}</h3>
+          <h5 class="card-title">Brand: ${phone.brand}</h5>
+          <h3>Name: ${phone.phone_name}</h3>
           <p class="card-text fw-bold">${phone.slug}</p>
+          <button id="detailsBtn" onclick="detailsButton()" class="btn btn-outline-success px-3 fw-bold ">Details</button>
         </div>
       </div>
         
